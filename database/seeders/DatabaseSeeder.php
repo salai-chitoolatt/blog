332f64080f0e28770204cf529fb89101006724d6
Salai Chit Oo Latt <salaichitoolatt@mpss.local>
2021-12-16 14:43:37 +0630

    finish Laravel 8 from Scratch - Section 4 - Episode 29

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,44 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
 
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            "name" => "Personal",
-            "slug" => "personal"
+        $user = User::factory()->create([
+            "name" => "John Doe"
         ]);
-
-        $family = Category::create([
-            "name" => "Family",
-            "slug" => "family"
+        
+        Post::factory(5)->create([
+            "user_id" => $user->id
         ]);
-
-        $work = Category::create([
-            "name" => "Work",
-            "slug" => "work"
-        ]);
-
-        Post::create([
-            "user_id" => $user->id,
-            "category_id" => $family->id,
-            "title" => "My Family Post",
-            "slug" => "my-first-post",
-            "excerpt" => "<p>Lorem ipsum dolar sit amet.</p>",
-            "body" => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas volutpat quis eros a ultrices. Sed vel nisi ut dolor cursus consequat. Aenean iaculis accumsan blandit. Suspendisse lobortis mattis posuere. Nullam a placerat augue, tempor dapibus lacus. Maecenas egestas ligula in ultrices venenatis. Donec et justo nec diam imperdiet tristique sed quis tortor. Nunc ultricies vehicula tristique. Etiam viverra semper tempor.</p>"
-        ]);
-
-        Post::create([
-            "user_id" => $user->id,
-            "category_id" => $work->id,
-            "title" => "My Work Post",
-            "slug" => "my-work-post",
-            "excerpt" => "<p>Lorem ipsum dolar sit amet.</p>",
-            "body" => "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas volutpat quis eros a ultrices. Sed vel nisi ut dolor cursus consequat. Aenean iaculis accumsan blandit. Suspendisse lobortis mattis posuere. Nullam a placerat augue, tempor dapibus lacus. Maecenas egestas ligula in ultrices venenatis. Donec et justo nec diam imperdiet tristique sed quis tortor. Nunc ultricies vehicula tristique. Etiam viverra semper tempor.</p>"
-        ]);
+    
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
 
-    $posts = Post::with('category')->get();
+    $posts = Post::latest()->with('category', 'author')->get();
 
 
     return view("posts", [
@@ -38,6 +39,13 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category', 'author'])
     ]);
 });
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts->load(['category', 'author'])
+    ]);
+});
+
