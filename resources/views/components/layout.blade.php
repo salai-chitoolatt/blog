178c1d8668fc575b840a5743a2672057ca8669e6
Salai Chit Oo Latt <salaichitoolatt@mpss.local>
2021-12-23 14:21:15 +0630

    Day 9 - Laravel 8 From Scratch
    - Courses
      * Section 1 - Prerequistics and Setup (DONE)
      * Section 2 - The Basics (DONE)
      * Section 3 - Blade (DONE)
      * Section 4 - Working With Databases (DONE)
      * Section 5 - Integrate the Design (In Progress)
        * Episode 31 - Convert the HTML and CSS to Blade (DONE)
        * Episode 32 - Blade Components and CSS Grids (DONE)
        * Episode 33 - Convert the Blog Post Page (DONE)
        * Episode 34 - A Small JavaScript Dropdown Detour (In Progress)

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,7 +17,7 @@
     <nav class="md:flex md:justify-between md:items-center">
       <div>
         <a href="#">
-          <img src="./images/logo.svg" alt="Laracasts Logo" width="165" height="16" />
+          <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16" />
         </a>
       </div>
       <div class="mt-8 lg:mt-0">
@@ -45,7 +45,7 @@
           px-10
           mt-16
         ">
-      <img src="./images/lary-newsletter-icon.png" alt="Newsletter Icon of Lary" class="mx-auto" style="width: 145px" />
+      <img src="/images/lary-newsletter-icon.png" alt="Newsletter Icon of Lary" class="mx-auto" style="width: 145px" />
       <h5 class="text-3xl">Stay in touch with the latest posts</h5>
       <p class="text-sm">Promise to keep the inbox clean. No bugs</p>
       <div class="mt-10">
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,22 +1,39 @@
 <x-layout>
-    <article>
-        <h1>
-            {{ $post->title }}
-        </h1>
+    <section class="px-6 py-4">
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
+                <div class="col-span-4 text-center">
+                    <img src="/images/illustration-1.png" alt="Ilustraion One" class="rounded-xl" />
+                    <p class="block mt-4 text-gray-400 text-xs">
+                        Published
+                        <time> {{ $post->created_at->diffForHumans() }} </time>
+                    </p>
 
-        <p>
-            By <a href="/authors/{{ $post->author->username }}">
-                {{ $post->author->name }}
-            </a> in 
-            <a href="/categories/{{ $post->category->slug }}">
-                {{ $post->category->name }}
-            </a>
-        </p>
+                    <div class="flex items-center justify-center text-sm mt-4">
+                        <img src="/images/lary-avatar.svg" alt="Lary avatar" />
+                        <div class="ml-3 text-left">
+                            <h5 class="font-bold">{{ $post->author->name }}</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-span-8">
+                    <div class="flex justify-between mb-6">
+                        <a href="/">Back to Posts</a>
+                        <div class="space-x-2">
+                            <x-category-button :category="$post->category" />
+                        </div>
+                    </div>
+                    <h1 class="font-bold text-4xl mb-3">
+                        {{ $post->title }}
+                    </h1>
 
-        <div>
-            {!! $post->body !!}
-        </div>
-    </article>
-
-    <a href="/">Go Back</a>
+                    <div class="space-y-6">
+                        {{
+                            $post->body
+                        }}
+                    </div>
+                </div>
+            </article>
+        </main>
+    </section>
 </x-layout>
